7441dd05f842da5db15c421f1c8aec2420ca031f
TJ Carney <tjcarney@stny.rr.com>
2018-01-02 16:02:18 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'TJ', last_name: 'Carney')
+Student.create(first_name: 'Sean', last_name: 'Carney')
+Student.create(first_name: 'Tom', last_name: 'Carney')
+Student.create(first_name: 'Tara', last_name: 'Carney')
+Student.create(first_name: 'Kathy', last_name: 'Carney')
